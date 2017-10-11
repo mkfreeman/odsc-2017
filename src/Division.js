e069b889c66194ae470c83e095cfe81068c93834
Michael Freeman <michael.k.freeman@gmail.com>
2017-10-11 15:25:37 -0700

    Set up division example

---
+++ src/Division.js
@@ -0,0 +1,82 @@
+// Division
+import React, {Component} from 'react';
+import Bubbles from './Bubbles';
+import * as d3 from 'd3';
+
+// Compute data
+const getConfig = function (stage) {
+    // Defaults
+    var circles = d3
+        .range(0, 20)
+        .map((d, i) => window.innerWidth / 2);
+
+    var fill = (d, i) => i % 2 == 1
+        ? '#26a69a'
+        : 'rgb(215, 81, 89)';
+    // Per stage
+    switch (stage) {
+        case 0:
+            fill = '#26a69a';
+            break;
+        case 1:
+            break;
+        case 2:
+            circles = d3
+                .range(0, 20)
+                .map(function (d) {
+                    var margin = window.innerWidth * .25
+                    var value = d % 2 == 1
+                        ? margin
+                        : window.innerWidth - margin;
+                    return value
+                })
+            default:
+            break;
+    }
+
+    return {
+        data: circles,
+        fill: fill,
+        duration: 500,
+        delay: (d, i) => {
+            return i * 100
+        },
+        ease: d3.easeCubic,
+        r: 12,
+        cx: (d) => d,
+        cy: (d, i) => 15 + i * (window.innerHeight - 70) / 20,
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+}
+
+class Division extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            progress: 0
+        }
+    }
+    handleKeyPress(event) {
+        if (event.keyCode == 37 || event.keyCode == 39) {
+            var change = event.keyCode == 37
+                ? -1
+                : 1;
+            var newState = this.state.progress + change;
+            this.setState({progress: newState});
+        }
+    }
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleKeyPress.bind(this), false);
+    }
+    render() {
+        console.log(this.state.progress)
+        var config = getConfig(this.state.progress);
+        return (
+            <div >
+                <Bubbles config={config}/>
+            </div>
+        )
+    }
+}
+export default Division;
