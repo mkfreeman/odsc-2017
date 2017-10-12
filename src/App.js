1637d19226eade78db65291b54bfcd6ec41c4218
Michael Freeman <michael.k.freeman@gmail.com>
2017-10-11 17:37:31 -0700

    Switch to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import Division from './Division';
 import Approach from './Approach';
 import './App.css';
